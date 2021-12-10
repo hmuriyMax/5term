a5646244af53ee2b8085738a74a2fd4fce393daf
max <mshchemilkin@gmail.com>
2021-12-10 22:32:51 +0300

    graph lab8 done [10.12.21]

--- methods/lab8/main.py
+++ methods/lab8/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def analiticalAns(x):
@@ -48,44 +49,86 @@ def getAnyX(x, UDots, h, a, b):
         left = right
         right += h
         i += 1
-        if right > b:
+        if left > b:
             i -= 1
             break
-    y1 = UDots[i]
-    y0 = UDots[i - 1]
+    if i + 1 > len(UDots) - 1:
+        i -= 1
+        right = b
+        left = b - h
+    y1 = UDots[i + 1]
+    y0 = UDots[i]
     x0 = left
     x1 = right
     return (y1 - y0) / h * x + (y0 * x1 - y1 * x0) / h
 
 
-A = 2
-B = 4
-Ua = -1
-Ub = -6
-eps = 1e-4
-N = 2
 
 
-def getEps(A, B, Ua, Ub, N):
-    h = (B-A)/N
-    x = np.linspace(A, B, N+1)
-    a = [h - 2 for _ in range(N + 1)]
-    b = [4 for _ in range(N + 1)]
-    c = [h + 2 for _ in range(N + 1)]
-    d = [2 * h ** 2 * f(x[i]) for i in range(N + 1)]
-
-    udots = getUVec(getCoefs(a,b,c,d,N), Ua, Ub)
-    deltas = []
-    for xi in x:
-        delta = analiticalAns(xi) - getAnyX(xi, udots, h, A, B)
-        deltas.append(delta)
-
-    return max(deltas)
+def k1(x):
+    return 6-x
 
 
-delta = getEps(A, B, Ua, Ub, N)
-while delta > eps:
-    N *= 2
-    delta = getEps(A, B, Ua, Ub, N)
-    print(N, (B-A)/N)
-    print(delta)
+def k2(x):
+    return np.sqrt(x+3)
+
+
+def k3(x):
+    return 16
+
+
+def k(x, prstn):
+    if x < 0 or x > 3:
+        return 0
+    elif x <= 1:
+        return prstn[0](x)
+    elif x <= 2:
+        return prstn[1](x)
+    else:
+        return prstn[2](x)
+
+
+A = 0
+B = 3
+Ua = 5
+Ub = 1
+N = 50
+x0 = 2.2
+
+
+def GetUdots(A, B, Ua, Ub, N, prstn):
+    h = (B - A) / N
+    x = np.linspace(A, B, N + 1)
+    a = [-k(x[i] - h / 2, prstn) for i in range(N + 1)]
+    b = [k(x[i] + h / 2, prstn) + k(x[i] - h / 2, prstn) for i in range(N + 1)]
+    c = [-k(x[i] + h / 2, prstn) for i in range(N + 1)]
+    d = [h ** 2 * f(x[i]) for i in range(N + 1)]
+
+    d[0] = Ua
+    d[N] = Ub
+    b[0] = 1
+    c[0] = 0
+    a[N] = 0
+    b[N] = 1
+
+    return getUVec(getCoefs(a, b, c, d, N), Ua, Ub)
+
+
+plt.figure(figsize=(9, 9))
+funcs = [k1, k2, k3]
+prstn = []
+for i in range(3):
+    prstn.append(funcs[i])
+    for j in range(3):
+        if j == i:
+            continue
+        prstn.append(funcs[j])
+        for l in range(3):
+            if l == i or l == j:
+                continue
+            x = np.linspace(A, B, N + 1)
+            prstn.append(funcs[l])
+            udots = GetUdots(A, B, Ua, Ub, N, prstn)
+            y = [getAnyX(xi, udots, h, A, B) for xi in x]
+
+plt.legend()
